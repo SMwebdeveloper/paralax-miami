6442594c7b18535b0b1b949181f09e85f3c92d17
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2022-03-01 21:02:32 +0500

    site finished

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
      freeMode: true,
      centeredSlides:true,
      mousewheel:true,
+     parallax:true,
      breakpoints:{
          0:{
              slidesPerView:2.5,
@@ -13,3 +14,26 @@
          }
      }
  })
+
+ const sliderBg = new Swiper('.slider_bg', { 
+    centeredSlides:true,
+    parallax:true,
+    spaceBetween:60,
+    slidesPerView:3.5
+})
+
+sliderMain.controller.control = sliderBg
+
+document.querySelectorAll('.slider_item').forEach(item => {
+    item.addEventListener('click', () => {
+     item.classList.toggle('opened')
+    })
+})
+
+
+let desc = document.querySelector('.description')
+
+
+sliderMain.on('slideChange', () => {
+    sliderMain.activeIndex > 0 ? desc.classList.add('hidden') : desc.classList.remove('hidden')
+})
